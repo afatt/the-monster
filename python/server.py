2799af025d2909f35f6509df02c8cabe68521580
austinfatt <afatt90@gmail.com>
2021-06-26 12:45:22 -0400

    Adds: server.py to connect to anvil client via uplink Adds: .gitignore

---
+++ python/server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import yaml
+import anvil.server
+
+# opens the yaml file and sets variable containing the anvil 'uplink key'
+with open('secrets.yaml') as yml:
+	contents = yaml.load(yml, Loader=yaml.FullLoader)
+	anvil_key = contents['api_keys']['anvil']
+ 
+anvil.server.connect(anvil_key)
+
+# this is how you make a function callable by the anvil client
+@anvil.server.callable
+def from_uplink(name):
+  print("Hello from the uplink, %s!" % name)
+
+# maintains the connection to the client even when client disconnects
+anvil.server.wait_forever()
